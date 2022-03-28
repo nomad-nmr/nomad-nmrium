c86318c173a22ccff6b9eca376b5553c3b05d44d
tomlebl <tl12@st-andrews.ac.uk>
2022-03-28 09:52:36 +0100

    minor production fixes

--- axios-instance.js
+++ axios-instance.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // /api has to be added in to the route to allow host both front and back end using single nginx server in production environment
-  baseURL:
-    import.meta.env.MODE === 'production'
-      ? import.meta.env.VITE_APP_API_URL + '/api'
-      : import.meta.env.VITE_APP_API_URL
+  // nomad-nmrium needs to use /data route which is set to handle large files
+  // api is only use by nomad-frontend to avoid confusion with React Router using same routes
+  baseURL: import.meta.env.VITE_APP_API_URL
 })
+//     import.meta.env.MODE === 'production'
+//       ? import.meta.env.VITE_APP_API_URL + '/api'
+//       : import.meta.env.VITE_APP_API_URL
+// })
 
 export default instance

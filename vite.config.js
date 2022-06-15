557c4abe4dccbb9cce7b52853eed336f5de8ae76
tomlebl <tl12@st-andrews.ac.uk>
2022-06-15 11:19:28 +0100

    dev-env dockerised

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: './config/'
+  envDir: './config/',
+  server: {
+    host: true,
+    port: 3000
+  }
 })
